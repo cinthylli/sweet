52f8f7a548ae7dfbb937697cc16487947d548338
Cinthya Cabanzo <cinthya.cabanzo@gmail.com>
2020-07-19 14:11:28 -0500

    footer terminado

--- sweet/src/components/pages/Footer.jsx
+++ sweet/src/components/pages/Footer.jsx
@@ -1,29 +1,41 @@
 import React from "react";
-import "../../styles/footer.css"
+import "../../styles/footer.css";
 
 export default function Footer() {
   return (
     <footer>
       <ul className="footer-left">
         <li>
-          <a href="/">Terminos & Condiciones</a>
+          <a href="/" target="blank">
+            Terminos & Condiciones
+          </a>
         </li>
         <li>
-          <a href="/">Nosotros</a>
+          <a href="/" target="blank">
+            Nosotros
+          </a>
         </li>
         <li>
-          <a href="/">Promociones</a>
+          <a href="/" target="blank">
+            Promociones
+          </a>
         </li>
       </ul>
       <ul className="footer-right">
         <li>
-          <a href="/">Facebook</a>
+          <a href="/" target="blank">
+            Facebook
+          </a>
         </li>
         <li>
-          <a href="/">Twitter</a>
+          <a href="/" target="blank">
+            Twitter
+          </a>
         </li>
         <li>
-          <a href="/">Instagram</a>
+          <a href="/" target="blank">
+            Instagram
+          </a>
         </li>
       </ul>
     </footer>
